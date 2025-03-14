aefa6cb22397f4c5c3e777528ded38c21e639a7b
LP <luhis9929@gmail.com>
2025-03-14 15:56:21 -0500

    ruta de salida de HomeScreeM

---
+++ lib/Presentacion/screens/movies/home_screen_m.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenM extends StatelessWidget {
+
+  // ruta de salida de HomeScreeM
+  static const name = 'home-screen-m';
+
+  const HomeScreenM({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

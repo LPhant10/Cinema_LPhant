8707028a9c91e9817dc886b8b75c6339b50c9e12
LP <luhis9929@gmail.com>
2025-03-14 16:35:05 -0500

    conf/domain/datasources/movies_data_sources.dart

---
+++ lib/config/domain/datasources/movies_data_source.dart
@@ -0,0 +1,15 @@
+/* abstac cuando no se quiere instancias de ella
+aqui en el data source definimos como lucen los origenes de datos  que
+pueden trar peliculas pueden ser de MovieDB u otro, mi API propia, de un 
+Json, de donde sea.
+Definiremos los metodos que estas clases tendran o que puedo llamar para
+traer esa data*/
+
+import 'package:cinema_lphant/config/domain/entities/movie.dart';
+
+abstract class MoviesDataSource {
+
+  Future<List<Movie>> getNowPlating({int page = 1});
+
+
+}

ec333784a23764253ad329ed67fe17d5e6f22197
LP <luhis9929@gmail.com>
2025-03-14 16:37:50 -0500

    conf/domain/repositories/movies_repository.dart

---
+++ lib/config/domain/repositories/movies_repository.dart
@@ -0,0 +1,12 @@
+/* Los respositorios son los que llamaran al data surce,
+   El repositorio me permitira cambiar el data source el origenes
+   de datos  */
+
+import 'package:cinema_lphant/config/domain/entities/movie.dart';
+
+abstract class MoviesRepository {
+
+  Future<List<Movie>> getNowPlating({int page = 1});
+
+
+}

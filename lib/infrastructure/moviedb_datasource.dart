29cb5b6dd9df1444eb182a94e700ad7d2abca541
LP <luhis9929@gmail.com>
2025-03-14 17:48:03 -0500

    creacion de cliente de peticiones http para moviedb

---
+++ lib/infrastructure/moviedb_datasource.dart
@@ -0,0 +1,29 @@
+/* Especializado y nace para interactuar unicamente con moviedb */
+
+import 'package:cinema_lphant/config/constants/environment.dart';
+import 'package:cinema_lphant/domain/datasources/movies_data_source.dart';
+import 'package:cinema_lphant/domain/entities/movie.dart';
+import 'package:dio/dio.dart';
+
+class MoviedbDatasource extends MoviesDataSource{
+
+  final dio = Dio(BaseOptions(
+    baseUrl: 'https://api.themoviedb.org/3',
+    queryParameters: {
+      'api_key': Environment.theMovieDBkey,
+      'language': 'es-MX'
+    }
+  ));
+
+  
+  @override
+  Future<List<Movie>> getNowPlating({int page = 1}) async {
+
+   
+
+   return[];
+  }
+
+
+
+}

5179038525d5367afdb0cb3b3452fed6148e1c0d
LP <luhis9929@gmail.com>
2025-03-19 12:54:49 -0500

    NowPlaying Provider y Notifier

--- lib/Presentacion/providers/movies/movies_repository_provider.dart
+++ lib/Presentacion/providers/movies/movies_repository_provider.dart
@@ -5,8 +5,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Este repositorio es inmutable
 final movieRepositoryProvider = Provider((ref) {
-  
-
   return MovieRepositoryImpl(MoviedbDatasource());
 }
 );

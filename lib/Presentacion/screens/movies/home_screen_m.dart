e1dd0fffdc563b4075e0012d56029c641733573e
LP <luhis9929@gmail.com>
2025-03-19 13:23:28 -0500

    Mostrar peliculas en pantalla

--- lib/Presentacion/screens/movies/home_screen_m.dart
+++ lib/Presentacion/screens/movies/home_screen_m.dart
@@ -1,5 +1,7 @@
 //import 'package:cinema_lphant/config/constants/environment.dart';
+import 'package:cinema_lphant/Presentacion/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreenM extends StatelessWidget {
 
@@ -12,12 +14,49 @@ class HomeScreenM extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(title: Text('Hola Luis'),),
-      body: Placeholder(
-        
-      )
+      body: _HomeView()
       /* Center(
         child: Text(Environment.theMovieDBkey),
       ) */
     );
 }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+
+    // ref(//.notifies).loadNextPage()
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    // if (nowPlayingMovies.length == 0) return CircularProgressIndicator();
+
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
+    );
+  }
 }
